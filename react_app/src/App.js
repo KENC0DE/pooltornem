d7b1103c79a7c9e70e79f0d5cf277273f94488a4
KENC0DE <kenansameseret@gmail.com>
2024-05-29 13:04:30 +0530

    add more components

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import Dashboard from './Dashboard';
+import "./App.css";
+import Dashboard from "./Dashboard";
+import Landingpage from "./Landingpage";
+import Register from "./Register";
+import Login from "./Login";
+import PlayerTable from "./PlayerTable";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Contact />
     </div>
   );
 }
---
+++ react_app/src/Tournament.js
@@ -0,0 +1,13 @@
+import {
+  SingleEliminationBracket,
+  DoubleEliminationBracket,
+  Match,
+  SVGViewer,
+  createTheme,
+} from "@g-loot/react-tournament-brackets";
+
+const Tournament = () => {
+  return <div></div>;
+};
+
+export default Tournament;
